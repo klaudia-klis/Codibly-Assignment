48253425ef9c3f10093d70bb500206bf4ad17180
Klaudia <kklis@interia.pl>
2023-01-30 17:37:41 +0700

    With useEffect

--- src/App.js
+++ src/App.js
@@ -1,80 +1,73 @@
 import React, { useState, useEffect } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-import Table from "./Table";
 import './App.css';
 
 function App() {
-  const [show, setShow] = useState(false);
   const [error, setError] = useState(null);
   const [products, setProducts] = useState([]);
-  const [input, setInputNumber] = useState([]);
-  
-  const handleClose = () => setShow(false);
-  const handleShow= () => setShow(true);
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    fetch("https://reqres.in/api/products")
+    fetch(`https://reqres.in/api/products?per_page=5&page=${page}`)
       .then((response) => response.json())
       .then((response) => {
-        setProducts(response.data);
+        setProducts([...response.data]);
         setError(null);
       })
       .catch(setError);
   }, []);
   
-  if (error) {
-    return 
-      <p>"An error occurred"</p>
-  }
-  
   const handleChange = (e) => {
     const value = e.target.value.replace(/\D/g, "");
-    setInputNumber(value);
-    if (!e.target.value) return setInputNumber(value)
-    const resultsArray = products.filter(product => product.id == e.target.value);
-  };
+    setSearch(value);
+  }
   
   return (
     <div className="main">
       <h1>Codibly Application</h1>
       
-      <div className="searchbox">
-        <input
-          value={input}
-          onChange={handleChange}
-          placeholder="Search here" />
-      </div>
+     <input 
+       className="search__input"
+       type="text"
+       id="search"
+       onChange={handleChange}
+       value={search}
+       placeholder="Search"
+     />
     
       <table>  
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Year</th>
-        </tr>
-      
-      {products.map((val, key) => {
-        return (
-          <tr key={key} onClick={handleShow}>
-            <td>{val.id}</td>
-            <td>{val.name}</td>
-            <td>{val.year}</td>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Year</th>
           </tr>
-        )
-      })}
+        </thead>
+        
+        <tbody>
+          {products.filter(function (product) {
+            if (search) {
+              return product.id === parseInt(search, 10)  
+            } else {
+              return true;
+            }
+            }).map((val, key) => {
+            return (
+              <tr key={key}>
+                <td>{val.id}</td>
+                <td>{val.name}</td>
+                <td>{val.year}</td>
+              </tr>
+            )
+          })}
+        </tbody>
       </table>
-     
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Name</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={handleClose}>Close</Button>
-        </Modal.Footer>
-      </Modal>
       
+      <button onClick={setPage} value={2}>
+        Click me!!!
+      </button>
     </div>
   );
 }
